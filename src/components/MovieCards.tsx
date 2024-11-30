a00c1486f83f902a6fa6b6ff34e6f0d8f3eb33d9
Captain Hook <Tooriskygh@gmail.com>
2024-11-30 19:54:33 +0000

    updates

--- src/components/MovieCards.tsx
+++ src/components/MovieCards.tsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { FaRegStar, FaClock, FaInfo } from 'react-icons/fa6';
-import { getTrendingMovies, getMovieDetails, Movie } from '/src/library/tmdb.ts';
+import { getTrendingMovies, getMovieDetails, Movie } from '../library/tmdb.ts';
 
 // Extended movie interface to include additional details
 interface DetailedMovie extends Movie {
